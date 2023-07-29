ecc11179088789e4d9eeae9fcf2aac01c1efb2f2
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-29 11:20:06 +0530

    exporting data for lecture -11.1

---
+++ lecture-11.2.js
@@ -0,0 +1,11 @@
+//This is lecture 11 of making a simple and basic api 
+//Creating array of objects 
+const data=[
+    {name:"Rahul Kumar",
+     email:"rahulkumarjha58978@gmail.com"},
+    {name:"Shashank Yadav",
+     email:"yshashank024@gmail.com"},
+    {name:"Rohit Kumar",
+     email:"rohitkumarjha6407@gmail.com"},
+]
+module.exports=data;   //exporting the data 
