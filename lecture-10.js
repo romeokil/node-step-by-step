496d2ca0859755f658d6527ee052e4cecc40a82e
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-29 11:17:17 +0530

    installation of nodemon package

---
+++ lecture-10.js
@@ -0,0 +1,10 @@
+//This is lecture-10 ->installation of nodemon package
+//Node js is async in nature not wait for line to execute will go ahead once the late process completes its execution 
+//Its output will be shown in between other processes.
+//For nodemon install locally ->npm i nodemon
+//For nodemon install globally->npm i nodemon -g
+
+const colors=require('colors')
+
+console.log("Hello Rahul".bgBlue)
+console.log("Kaisa hai bhai ".bgCyan)
