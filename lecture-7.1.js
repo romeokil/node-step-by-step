016d8557e3352f99148593e367ad749c9cd109df
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-29 11:14:38 +0530

    color package/modules import

---
+++ lecture-7.1.js
@@ -0,0 +1,4 @@
+//This is lecture-7 index.js file used to test colors package and yes it is working properly.
+const colors=require('colors')
+console.log("hello rahul".blue);
+console.log("Welcome to the external package colors".bgGreen)
