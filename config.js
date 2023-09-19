30a35279c7e380686f27b46d5da7bd0907e56386
romeokil <123149490+romeokil@users.noreply.github.com>
2023-09-19 10:56:41 +0530

    extension of lec-40.js mongoose connection
    esme hmlog mongoose connection established kr rhe hai.

---
+++ config.js
@@ -0,0 +1,5 @@
+//This is lecture no 41 in which we are making connection es lecture me hmlog get api ka use krege
+//Pehle bhi use kiye hue hai lekin isme ye scene hai ki npm packgae install krke kiye the 
+//But es waaale me hmlog mongoose waale se try krege ye saara api.
+const mongoose=require('mongoose')
+mongoose.connect("mongodb://127.0.0.1:27017/e-comm")
