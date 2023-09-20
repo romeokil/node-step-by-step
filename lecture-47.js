b57b2f4b1407b4ad49a9b961b36525e4794dff5b
romeokil <123149490+romeokil@users.noreply.github.com>
2023-09-20 18:49:31 +0530

    repl in nodejs
    repl mtlb read,eval,print,loop mtlb esme hmlog command line me likjhna sikhe hai koi agr koi function execute krna h toh kaise hoga.

---
+++ lecture-47.js
@@ -0,0 +1,10 @@
+//This is lecture no 47.js in which we study about repl(basic but important).
+//REPL basically stands for read,eval,print,loop
+//Es lecture me basically command prompt pe likhna sikha rha ai.
+//ek line execution me 2+2 or 4-7 aise chizen btae
+//dekho aise toh ek hi line execute hota hai lekin agr tm editor waale mode me likhega toh 
+//tm phir pura 3-4 line ko execute kr skta hai.
+//multiple execution me js function kaise execute kr skte hai wo btaya.
+//function apple(){
+//console.log("Akriti chutiya hai")
+//}
